773189c196adc472dc78976117b57f5773ad379b
NoLifeKing <webmaster@itssimple.se>
2022-06-06 10:42:05 +0200

    fix: Forgot to change to uint on some endpoints

--- Categories.cs
+++ Categories.cs
@@ -5,7 +5,7 @@ namespace CurseForge.APIClient
 {
     public partial class ApiClient
     {
-        public async Task<GenericListResponse<Category>> GetCategoriesAsync(int? gameId = null, int? classId = null)
+        public async Task<GenericListResponse<Category>> GetCategoriesAsync(uint? gameId = null, uint? classId = null)
         {
             return await GET<GenericListResponse<Category>>("/v1/categories", ("gameId", gameId), ("classId", classId));
         }
